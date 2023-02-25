b974850459149a46484695f7dfd817f0adb1b448
ayushjik <112191793+ayushjik@users.noreply.github.com>
2023-02-25 20:28:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const io = require("socket.io")(server, {
 
 app.use(cors());
 app.use(bodyParser.json());
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
 	res.send('Running');
