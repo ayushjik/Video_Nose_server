6efa163c7c4659a4d6c0b38db99d9c7fbbe2ded1
ayushjik <112191793+ayushjik@users.noreply.github.com>
2023-03-03 19:19:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -141,10 +141,10 @@ let Rec_id_check=[];
 	});
 // =========End Socket=====================================================
 	
-// 	socket.on("Step_count_Id",({Doc_id,Step_Count})=>{
-// 		io.to(Doc_id).emit('Step_count_value_rec', {scount:Step_Count});
-// 		console.log("Doc_id:- "+ Doc_id, "STep_count= "+Step_Count)
-// 	})
+	socket.on("Step_count_Id",({Doc_id,Step_Count})=>{
+		io.to(Doc_id).emit('Step_count_value_rec', {scount:Step_Count});
+		console.log("Doc_id:- "+ Doc_id, "STep_count= "+Step_Count)
+	})
 	
 
 // =========Start ZOOM=====================================================
