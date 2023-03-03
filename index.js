f4f6342011a539951c9fdcf02989f90ed6fdf905
ayushjik <112191793+ayushjik@users.noreply.github.com>
2023-03-03 15:43:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,11 +18,12 @@ const PORT = process.env.PORT || 5000;
 app.get('/', (req, res) => {
 	res.send('Running');
 });
+let Step_Count
 app.get('/StepCount', (req, res) => {
 	const value = req.query.data;
+	Step_Count=value;
 	res.send(`StepCount Value= ${value}`);
 	console.log(`Received value: ${value}`);
-
 	// res.status(200).send('Value received');
 });
 // if (cluster.isPrimary) {
@@ -33,8 +34,11 @@ app.get('/StepCount', (req, res) => {
 // 	  cluster.fork();
 // 	}
 
+console.log("Step_Count= "+Step_Count)
+
 io.on("connection", (socket) => {
 	socket.emit('me',socket.id);
+	socket.emit('step_count',Step_Count);
 
 	socket.on("disconnect", () => {
 		socket.broadcast.emit("callEnded")
@@ -91,7 +95,6 @@ let Rec_id_check=[];
 // 	io.adapter(io_redis({ host: '127.0.0.1', port: 6380 }));
 
 	io.on("connection", (socket) => {
-	console.log("connected To Worker:-"+cluster.worker.id);
 
 	// =============================?
 	// client.set('key', 'value',  Redis.print);
@@ -149,87 +152,4 @@ let Rec_id_check=[];
 // =========Start ZOOM=====================================================
 	});
 	server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
-	console.log(`Worker ${process.pid} started in last`);
 // }
-
-
-
-
-// var fs = require('fs');
-// // var http = require('http');
-// var https = require('https');
-// const app = require("express")();
-// // const server = require("http").createServer(app);
-// // var httpServer = http.createServer(app);
-// var privateKey  = fs.readFileSync('../private.key', 'utf8');
-// var certificate = fs.readFileSync('../primary.crt', 'utf8');
-// var credentials = {key: privateKey, cert: certificate};
-// var httpsServer = https.createServer(credentials, app);
-
-
-// const Doc=["rSchn6HysAbtHYsmAAAB","GfNQy2R-uaerwIuxAAAD","0J9SsORqPazwGHVUAAAB"];
-// const cors = require("cors");
-
-// const io = require("socket.io")(httpsServer, {
-// 	cors: {
-// 		origin: "*",
-// 		methods: [ "GET", "POST" ]
-// 	}
-// });
-
-// app.use(cors());
-
-// app.get('/', (req, res) => {
-// 	res.send('Running');
-// });
-
-// io.on("connection", (socket) => {
-// 	socket.emit('me',socket.id);
-
-// 	socket.on("disconnect", () => {
-// 		socket.broadcast.emit("callEnded")
-// 	});
-
-// 	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
-// 		console.log("callUser"+userToCall+" SignalData:-"+signalData+" from:-"+from+" name:-"+name)
-// 		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
-// 	});
-
-// 	socket.on("answerCall", (data) => {
-// 		console.log("data:-"+data +" data.signal:-"+data.signal)
-// 		io.to(data.to).emit("callAccepted", data.signal)
-// 	});
-
-
-// // =========Start Socket=====================================================
-// 	socket.on("socketId",(id)=>{
-// 		// socket.broadcast.emit('socketId',id)
-// 		io.to(id).emit(socket.id);
-// 		console.log("Socket:- "+ socket.id)
-// 	});
-
-// let Rec_id_check=[];
-// 	socket.on("Rec_socketId",(Rec_id)=>{
-// 		// socket.broadcast.emit('socketId',id)
-// 		io.to(socket.id).emit('socket_client', Rec_id);
-// 		Rec_id_check=socket.id;
-// 		console.log("Mecheck ",Rec_id +" Me:- "+ Rec_id_check)
-// 	});
-// // =========End Socket=====================================================
-
-
-// // =========Start ZOOM=====================================================
-// 	socket.on("send_message",({mecheck,Value})=>{
-// 		// socket.broadcast.emit('rec_message',Value)
-// 		io.to(mecheck).emit("rec_message",Value);
-// 		console.log("MeCheck= "+mecheck +" Value= "+ Value)
-// 	});
-// 	// socket.on("rec_message",(rec_data2)=>{
-// 	// 	// socket.broadcast.emit('socketId',id)
-// 	// 	io.to(Rec_id).emit(rec_data2);
-// 	// 	console.log("message ",rec_data2 +"sender_Socketmsg:- "+ socket.id+"Rec_Socketmsg:- "+ Rec_id)
-// 	// });
-// // =========Start ZOOM=====================================================
-// });
-// // httpServer.listen(5001, () => console.log(`Server is running on port 5001`));
-// httpsServer.listen(5000, () => console.log(`Server is running on port 5000`));
